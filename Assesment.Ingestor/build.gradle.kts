74750316b98f5ff48fa59ffa91c20d41bfa5ea56
Ivan <35352131+Rasmus715@users.noreply.github.com>
2025-11-08 10:45:36 +0300

    Introduce Assessment.Ingestor service & RabbitMQ instance & Queue (#1

--- Assesment.Ingestor/build.gradle.kts
+++ Assesment.Ingestor/build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("com.squareup.okhttp3:okhttp:5.3.0")
+    implementation("com.squareup.moshi:moshi:1.15.2")
+    implementation("com.rabbitmq:amqp-client:5.21.0")
+    implementation("org.slf4j:slf4j-simple:2.0.17")
 }
 
 tasks.test {
